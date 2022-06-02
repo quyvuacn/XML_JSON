f0cdb516d1a28acc737c8f6605861c0fc1044cc9
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-06-02 11:34:00 +0700

    fix

--- Json/js/weather.js
+++ Json/js/weather.js
@@ -3,6 +3,9 @@ const weatherApp = angular.module('weatherApp',[])
 
 weatherApp.controller('WeatherCtrl', function($scope,$http){
     $http.get(API_WEATHER).then(function({data}){
+            if(data){
+                document.querySelector('.weather').style.display = 'block'
+            }
             $scope.keySearch =''
             $scope.name = "Ha Noi"
             $scope.country = "VN"
@@ -16,6 +19,7 @@ weatherApp.controller('WeatherCtrl', function($scope,$http){
             let q = $scope.keySearch.split(' ').join('%20')
             $http.get(`https://api.openweathermap.org/data/2.5/weather?&q=${q}&appid=eb8a0f01c55e8a90eae383cac5461c16&units=metric`)
                 .then(({data})=>{
+                    console.log(data)
                     $scope.icon = data.weather[0].icon
                     $scope.img_src = `https://openweathermap.org/img/wn/${$scope.icon}@4x.png`
                     $scope.current=data.main.temp
