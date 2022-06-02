d12d3d892e11cd7589f90923f5d9dfb9940516d4
quyvuacn <87663363+quyvuacn@users.noreply.github.com>
2022-06-02 08:22:01 +0700

    update

--- Json/js/weather.js
+++ Json/js/weather.js
@@ -1,6 +1,6 @@
 const API_WEATHER = 'https://api.openweathermap.org/data/2.5/weather?lat=21.027763&lon=105.834160&appid=eb8a0f01c55e8a90eae383cac5461c16&units=metric'
 const IMG_URL = 'https://encrypted-tbn0.gstatic.com/images?q=tbn:ANd9GcQaUJwc5BDIDZAkUPWqKoHnFS96y1Gbx52_6w&usqp=CAU'
-var weatherApp = angular.module('weatherApp',[])
+const weatherApp = angular.module('weatherApp',[])
 
 weatherApp.controller('WeatherCtrl', function($scope,$http){
     
@@ -11,5 +11,7 @@ weatherApp.controller('WeatherCtrl', function($scope,$http){
         $scope.temp_min = data.main.temp_min
         $scope.wind_speed = data.wind.speed
         $scope.clouds = data.clouds ? data.clouds.all : undefined
+    }).catch(err=>{
+        console.log(err)
     })
 })
